3dd90b1a16add24d7c674de5a297f199c5510596
sushantgangwar7300-sys <sushantgangwar7300@users.noreply.github.com>
2026-09-11 13:41:45 +0530

    chore: add push helper script

---
+++ push-to-github.bat
@@ -0,0 +1,9 @@
+@echo off
+title Push Kai Prakriti to GitHub
+echo ===================================================
+echo Pushing Kai Prakriti repository to GitHub...
+echo Remote: git@github.com:sushantgangwar7300-sys/ESG-consultancy.git
+echo ===================================================
+
+git push -u origin main
+pause
